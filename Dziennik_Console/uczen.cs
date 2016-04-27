c13f6a6c493a3e935a48275c418d963be28001fb
Borys <Borys@BORYS-KOMPUTER>
2016-04-27 17:45:38 +0200

    Add project files.

---
+++ Dziennik_Console/uczen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik_Console
+{
+    class Uczen
+    {
+        String imie;
+        String nazwisko;
+        int pesel;
+
+        Uczen(String imie, String nazwisko, int pesel)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.pesel = pesel;
+        }
+
+        public double Srednia(int suma_ocen)
+        {
+
+        }
+    }
+}
